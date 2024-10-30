d2edf6d0d78989347707a86b9ded3e5d0fe7f52e
李龙晟 <12903409+lifarsight@user.noreply.gitee.com>
2024-10-30 19:11:53 +0800

    提交部分代码

---
+++ src/main/java/middle/a10/FourSumCount.java
@@ -0,0 +1,26 @@
+package middle.a10;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FourSumCount {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        int length = nums1.length;
+        // 数字对应索引的map
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums4.length; i++) {
+
+        }
+        int count = 0;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                int n = j+1;
+                int m = length-1;
+                while (n < m) {
+
+                }
+            }
+        }
+        return count;
+    }
+}
