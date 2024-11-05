761fe8780d674a65234c239b9beb134b70c19155
李龙晟 <12903409+lifarsight@user.noreply.gitee.com>
2024-11-05 14:53:15 +0800

    初步实现非递归后序遍历

--- src/main/java/classical/tree/NoRecursionTraversal.java
+++ src/main/java/classical/tree/NoRecursionTraversal.java
@@ -56,11 +56,49 @@ public class NoRecursionTraversal {
         return result;
     }
 
+    /**
+     * 后序遍历
+     * @param root
+     * @return
+     */
+    public List<Integer> postorder(TreeNode root){
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        TreeNode p = root;
+        TreeNode lastVisited = null;
+        while (p!=null || !stack.empty()) {
+            if (p != null) {
+                stack.push(p);
+                p = p.left;
+            }else {
+                // 右子树存在，且右子树没访问过，访问右子树
+                // 右子树不存在，弹出本节点
+                TreeNode peek = stack.peek();
+                // 右节点不为空且peek的右节点没有被访问过
+                if (peek.right != null && lastVisited != peek.right) {
+                    p=peek.right;
+                    lastVisited = p;
+                }else {
+                    TreeNode pop = stack.pop();
+                    result.add(pop.val);
+                    p = pop.right;
+                    lastVisited = pop.right;
+                }
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         NoRecursionTraversal noRecursionTraversal = new NoRecursionTraversal();
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, null, 8, null, null, 6, 7, 9);
         TreeNode root = TreeNode.listToBinaryTree(list);
-        System.out.println(noRecursionTraversal.inorder(root));
-        System.out.println(noRecursionTraversal.preorder(root));
+//        System.out.println(noRecursionTraversal.inorder(root));
+//        System.out.println(noRecursionTraversal.preorder(root));
+        System.out.println(noRecursionTraversal.postorder(root));
     }
 }
