18f416e1555da492d5a3f2c0e9e313ff3acb4554
FaiSightLi <farsight@live.com>
2024-12-09 23:34:57 +0800

    完成递归版FlattenTree

--- src/main/java/middle/a30/FlattenTree.java
+++ src/main/java/middle/a30/FlattenTree.java
@@ -4,25 +4,41 @@ import common.TreeNode;
 
 public class FlattenTree {
     public void flatten(TreeNode root) {
+        // 开始遍历，当前节点就是root
         TreeNode curr = root;
         while (curr != null) {
-            // 找到第一个左节点
+            // 开始找当前节点的左节点的最右节点
             if (curr.left != null) {
                 TreeNode runner = curr.left;
-                // 开始找最右节点
                 while (runner.right != null) {
                     runner = runner.right;
                 }
-                // 找到最右节点了，将curr的右节点连到最右节点的右侧
+                // 找到最右节点了，开始链接
                 runner.right = curr.right;
-                // 将这个左子树放到curren的右节点
+                // 将左子树放到右子树位置
                 curr.right = curr.left;
                 // 将左子树置空
                 curr.left = null;
             }
-            // 在下一个节点（即右节点继续此逻辑）
+            // 继续展平下一个节点
             curr = curr.right;
         }
     }
 
+    public void flattenRecursion(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeNode left = root.left;
+        if (left != null) {
+            //找右节点
+            while (left.right != null) {
+                left = left.right;
+            }
+            left.right = root.right;
+            root.right = root.left;
+            root.left = null;
+        }
+        flattenRecursion(root.right);
+    }
 }
